82fe226f12b6cdd6dec16b3cbbff27b257175d38
Rahilsamani <rahilahmed1720@gmail.com>
2024-08-05 06:58:04 +0530

    add link

--- server/index.js
+++ server/index.js
@@ -13,7 +13,7 @@ const PORT = process.env.PORT || 5000;
 // Middleware
 app.use(
   cors({
-    origin: "https://rahilsamani.github.io/portfolio",
+    origin: "https://portfolio-rahil-ahmeds-projects-248c0603.vercel.app",
     credentials: true,
   })
 );
