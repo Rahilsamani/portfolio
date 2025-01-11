d7c2d7917ab4897e69809f3094568d795d3ad7c3
Rahil Ahmed <rahilahmed1720@gmail.com>
2025-01-11 23:12:25 +0530

    fix backend error

--- server/index.js
+++ server/index.js
@@ -41,8 +41,6 @@ const Contact = mongoose.model("Contact", contactSchema);
 
 const transporter = nodemailer.createTransport({
   host: process.env.MAIL_HOST,
-  port: process.env.MAIL_PORT || 587,
-  secure: process.env.MAIL_SECURE === "true",
   auth: {
     user: process.env.MAIL_USER,
     pass: process.env.MAIL_PASS,
